716b5ed1e9fe94b60fc166e2613b7228ca523045
Deanne Lee <missdeannelee@gmail.com>
2022-05-04 14:30:28 -0600

    commit of day 2 work

---
+++ dice-roller.js
@@ -0,0 +1,14 @@
+//generate a random number between 1 & 6
+Math.random();
+
+var diceRolled = Math.floor(Math.random() * 6) + 1;
+//make it go through the command line with array.prototype.join? 
+// process.argv.slice(2)??
+// for (let i = 0; i < args; i++) {
+  //results.push(Math.floor(Math.random() * 6) + 1)
+}
+//have any number of dice
+const dice = ['Die1', 'Die2', 'Die3'];
+console.log(dice.join( ));
+
+console.log(diceRolled);
---
+++ sum.js
@@ -0,0 +1,7 @@
+let sum1 = 7
+let sum2 = 9
+
+const args = process.argv ( function[sum1, sum2] ) ; {
+  return process.argv (sum1 * sum2);
+}
+console.log(args);
